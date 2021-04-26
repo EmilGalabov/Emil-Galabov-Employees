448d61228bc1a2fa898003235b68ff03db75153e
EmilGalabov <83216845+EmilGalabov@users.noreply.github.com>
2021-04-26 15:33:44 +0300

    Delete Emil-Galabov-Employees directory

--- Emil-Galabov-Employees/src/File.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface File {
-    List<String> readFile(String filePath) throws IOException;
-
-    void write(String content, String filePath) throws IOException;
-
-}
